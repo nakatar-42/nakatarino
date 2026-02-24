9b36adbd2eea94e2a3c67eb73015d12e25036fe0
nakat <nakat@nakatarino>
2026-02-24 21:36:52 +0900

    テスト確認

---
+++ kadai1/src/curriculum/a/Question1.java
@@ -0,0 +1,19 @@
+package curriculum.a;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		
+		byte byteNum = 10;
+		short shortNum = 100;
+		int intNum = 1000;
+		long longNum = 10000;
+		float floatNum = 9.5f;
+		double doubleNum = 10.5;
+		char letter = 'a';
+		String letters ="Hello";
+		boolean isBoolean = true;
+		
+		System.out.println("テスト中");
+	}
+}
